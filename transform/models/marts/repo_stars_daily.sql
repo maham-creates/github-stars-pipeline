8e641326c6ff89c4cc0a0347d76794d6a28fc207
Maham <mahamtulips@gmail.com>
2024-10-15 17:06:33 -0400

    Built_models_in_dbt

---
+++ transform/models/marts/repo_stars_daily.sql
@@ -0,0 +1,13 @@
+Select distinct 
+  repo_id, 
+  repo_name, 
+  date_trunc('day', event_date) as date_day, 
+  count(*) as star_count,
+  SUM(COUNT(*)) OVER (PARTITION BY repo_id ORDER BY date_trunc('day', event_date)) AS cumul_star_count
+from {{ ref("stg_gharchive") }} 
+where event_type = 'Watch'
+GROUP BY 
+  repo_id, 
+  repo_name, 
+  date_day
+
