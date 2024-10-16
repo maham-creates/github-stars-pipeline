81627b714d6b7c19f7fa6e6ba7020db445d30c1e
Maham <mahamtulips@gmail.com>
2024-10-16 16:18:02 -0400

    Removed Comments

--- transform/models/staging/stg_gharchive.sql
+++ transform/models/staging/stg_gharchive.sql
@@ -1,11 +1,3 @@
--- select
---   type as event_type,
---   actor.login as user,
---   repo.id as repo_id,
---   repo.name as repo_name,
---   created_at as event_date
--- from {{ source("gharchive", "src_gharchive") }}
-
 select
    replace(type,'Event','') as event_type, --remove 'Event' suffix
    actor.login as user,
