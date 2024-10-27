ffaf59b5dbbf08be779f8b4bf37499a4eae86bea
Maham <mahamtulips@gmail.com>
2024-10-26 23:23:03 -0400

    fixed mart code & materialization

--- transform/models/marts/repo_stars_daily.sql
+++ transform/models/marts/repo_stars_daily.sql
@@ -3,7 +3,7 @@ SELECT
   repo_name, 
   date_trunc('day', event_date) AS date_day, 
   COUNT(*) AS star_count,
-  SUM(COUNT(*)) OVER (PARTITION BY repo_id ORDER BY date_trunc('day', event_date)) AS cumul_star_count
+  SUM(star_count) OVER (PARTITION BY repo_id ORDER BY date_day) AS cumul_star_count
 FROM {{ ref("stg_gharchive") }} 
 WHERE event_type = 'Watch'
 GROUP BY 1, 2, 3
